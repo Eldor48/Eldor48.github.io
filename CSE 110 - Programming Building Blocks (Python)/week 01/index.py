bfd51e4c3ee478685ba17e4afdaf09163ed71833
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2021-09-12 21:15:38 -0400

    changes

---
+++ CSE 110 - Programming Building Blocks (Python)/week 01/index.py
@@ -0,0 +1,7 @@
+print("Hi! I am trying to get to know you a little bit more, I will ask you some questions.")
+print()
+print("What is your favorite color?")
+favColor = input()
+# favColor.lower()
+print("Woah, it's great your favorite color is " + favColor.lower() + " that is a big coincidence, you know? My favotire color is also " + favColor.lower())
+ 
