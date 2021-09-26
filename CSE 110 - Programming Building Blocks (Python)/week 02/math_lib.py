d4128dc10d4ad5a45e4cded4c527c47809dfc023
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2021-09-26 01:06:27 -0400

    changes to homework

---
+++ CSE 110 - Programming Building Blocks (Python)/week 02/math_lib.py
@@ -0,0 +1,31 @@
+print("Please enter the following:")
+print()
+
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb = input("verb: ")
+exclamation = input("exclamation: ")
+verb1 = input("verb: ")
+verb2 = input("Second verb: ")
+print()
+print("Your history is:")
+print()
+
+print(f"The other day, I was really in trouble. It all started when I saw a very\n{adjective} {animal} {verb} down the hallway. {exclamation.capitalize()}!  I yelled. But all \nI could think to do was to {verb1} over and over. Miraculously, \nthat caused it to stop, but not before it tried to {verb2} \nright in front of my family.")
+print()
+print("Guess what? There is a second part in the history:")
+print("Here is the second part!")
+print("But before I tell you the second part, I will need more information from you.")
+
+adjective1 = input("adjective: ")
+foods = input("food: ")
+color = input("color: ")
+animal1 = input("animal: ")
+verbFirst = input("verb: ")
+exclamation1 = input("exclamation: ")
+verbSecond = input("verb: ")
+
+print(f"Surprisingly  my family was able to outrun the {animal}, but something {adjective1} happened.\nOut of nowhere a {color} {animal1} appeared in our back, it seems it was eating some {foods} that was left on the road.\nIt was pretty clear that this creature was about to {verbFirst}.\nFrom my back I heard that my mom yelled {exclamation1.capitalize()},\nbut we were all fine since the {animal1} just {verbSecond} down the hill.")
+
+
+
