bd6d3514d6506e27296eb0d23003c043e5976dd1
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2021-09-30 10:04:24 -0400

    finished, I guess

--- CSE 110 - Programming Building Blocks (Python)/week 03/mealPriceCalculator.py
+++ CSE 110 - Programming Building Blocks (Python)/week 03/mealPriceCalculator.py
@@ -3,17 +3,25 @@ adultMealPrice = input("What is the price of a adult's meal?")
 howManyChildren = input("How many children are there?")
 howManyAdults = input("How many adults are there?")
 drinkPrice = input("What is the prince of a drink here?")
-
+saleTax = input("What is the sale tax rate?")
 tip = float(input("How much do you want to tip?"))
 drinkTotal = (float(howManyChildren) + float(howManyAdults)) * float(drinkPrice)
 
 childrenSubtotal = float(childMealPrice) * float(howManyChildren)
-
+childrenSubtotal = round(childrenSubtotal, 2)
 adultSubtotal = float(adultMealPrice) * float(howManyAdults)
+adultSubtotal = round(adultSubtotal, 2)
 
 subTotal = childrenSubtotal + adultSubtotal + tip + drinkTotal 
+subTotal = round(subTotal, 2)
+saleTaxRate = (float(saleTax)  * subTotal)  / 100
+saleTaxRate = round(saleTaxRate, 2)
+total = saleTaxRate + subTotal 
+total = round(total, 2)
 print()
 print(f"Children's meal: ${childrenSubtotal}")
 print(f"Adults' meal: ${adultSubtotal}")
 print(f"Tip ${tip}")
 print(f"Subtotal: ${subTotal}")
+print(f"Sales Tax: ${saleTaxRate}") 
+print(f"Total: ${total}")
