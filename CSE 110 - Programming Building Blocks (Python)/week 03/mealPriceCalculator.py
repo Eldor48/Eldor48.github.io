a111a2c72ec2b37c079f07091e532754fa3e2585
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2021-09-30 09:22:38 -0400

    fully Functional

--- CSE 110 - Programming Building Blocks (Python)/week 03/mealPriceCalculator.py
+++ CSE 110 - Programming Building Blocks (Python)/week 03/mealPriceCalculator.py
@@ -2,11 +2,18 @@ childMealPrice = input("What is the price of a child's meal?")
 adultMealPrice = input("What is the price of a adult's meal?")
 howManyChildren = input("How many children are there?")
 howManyAdults = input("How many adults are there?")
+drinkPrice = input("What is the prince of a drink here?")
+
 tip = float(input("How much do you want to tip?"))
+drinkTotal = (float(howManyChildren) + float(howManyAdults)) * float(drinkPrice)
+
 childrenSubtotal = float(childMealPrice) * float(howManyChildren)
 
 adultSubtotal = float(adultMealPrice) * float(howManyAdults)
 
-subtotal = childrenSubtotal + adultSubtotal + tip
-
-print(f"Subtotal: ${subtotal}")
+subTotal = childrenSubtotal + adultSubtotal + tip + drinkTotal 
+print()
+print(f"Children's meal: ${childrenSubtotal}")
+print(f"Adults' meal: ${adultSubtotal}")
+print(f"Tip ${tip}")
+print(f"Subtotal: ${subTotal}")
