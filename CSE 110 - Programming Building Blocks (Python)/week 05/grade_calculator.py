b3320084c7b03586ad4737f918223dcd58aab463
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2021-10-16 20:08:40 -0400

    grades

--- CSE 110 - Programming Building Blocks (Python)/week 05/grade_calculator.py
+++ CSE 110 - Programming Building Blocks (Python)/week 05/grade_calculator.py
@@ -6,24 +6,72 @@ regular_grade = "Cheers, you got an C"
 bad_grade = "Unfortunately, you didn't receive a good grade, you got a "
 worst_grade = "You got a"
 
-if grade >= 90 and grade <= 93: 
-   print("Congratulations, you got an -A")
+if grade >= 90 and grade <= 93:
+    print("Congratulations, you got an A-")
+    print()
+    print("Congratulations, you passed the course!")
 elif grade >= 94 and grade < 100:
     print("Congratulations, you got an A")
+    print()
+
+    print("Congratulations, you passed the course!")
 elif grade == 100:
     print("Congratulations, you got an A+")
+    print()
+
+    print("Congratulations, you passed the course!")
 
 elif grade >= 87 and grade <= 89:
     print("Congratulations, you got a B+")
+    print()
+
+    print("Congratulations, you passed the course!")
 elif grade >= 83 and grade <= 86:
-      print("Congratulations, you got a B")
+    print("Congratulations, you got a B")
+    print()
+
+    print("Congratulations, you passed the course!")
 elif grade >= 80 and grade <= 82:
-       print("Congratulations, you got a B")
+    print("Congratulations, you got a B-")
+    print()
+
+    print("Congratulations, you passed the course!")
+elif grade >= 77 and grade <= 79:
+    print("Congratulations, you got a C+")
+    print()
+
+    print("Congratulations, you passed the course!")
+elif grade >= 73 and grade <= 76:
+    print("Congratulations, you got a C")
+    print()
+
+    print("Congratulations, you passed the course!")
+elif grade >= 70 and grade <= 72:
+    print("Congratulations, you got a C-")
+    print()
+
+    print("Congratulations, you passed the course!")
+elif grade >= 67 and grade <= 69:
+    print("You got a D+")
+    print()
+    print("Unfortunately you did not pass the course.")
+
+
+elif grade >= 63 and grade <= 66:
+    print("You got a D")
+    print()
+
+    print("Unfortunately you did not pass the course.")
+elif grade >= 60 and grade <= 62:
+    print("You got a D-")
+    print()
+
+    print("Unfortunately you did not pass the course.")
 elif grade < 60:
-    print(f"{worst_grade} D")
+    print("You got an F")
+    print()
+
+    print("Unfortunately you did not pass the course.")
 
 else:
     print("This is not an valid grade")
-
-
-
