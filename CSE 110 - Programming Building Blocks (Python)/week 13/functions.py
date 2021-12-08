04dfcc3974a52ed8f90205e460b65ea553c6867f
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2021-12-08 01:04:40 -0400

    commit

---
+++ CSE 110 - Programming Building Blocks (Python)/week 13/functions.py
@@ -0,0 +1,13 @@
+# def printDate():
+#     import datetime
+#     print(datetime.datetime.now())
+
+# printDate()
+
+# def fullnanme(first, last):
+#     print(first, last)
+
+def getInitial(first, last):
+    print (first[0] + last[0])  # this is the same as the line above 
+
+getInitial("John", "Smith")
