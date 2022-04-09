e326196fdcd391512cd38822aab3bfc10960c90e
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2022-04-09 01:12:19 -0400

    main

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const result = kate + jean;
 
 
 result;    
+
+quizzes = 11.3; 
+
+exams = 47.3;
+
+attendance = 5;
+
+projects = 20.83; 
+
+totalgrade = (quizzes * 0.1) + (exams * 0.3) + (attendance * 0.2) + (projects * 0.5);
+
+console.log(totalgrade);  
+
+const grade = totalgrade; 
+
+grade;
