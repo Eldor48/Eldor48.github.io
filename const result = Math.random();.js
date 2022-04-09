01f9f21f881ea348c7dacb9f1ae68cda8db100cb
Eldor488 <77108425+Eldor488@users.noreply.github.com>
2022-04-09 01:13:45 -0400

    main

---
+++ const result = Math.random();.js
@@ -0,0 +1,29 @@
+const result  = Math.random();
+
+result;
+
+
+
+const username = {name: "John", age: "23", lastname: "Doe", email: "example@gmmail.com", password: "1234567890"}; 
+
+console.log(
+    username.name + " " + username.lastname + " " + username.age + " " + username.email + " " + username.password //?
+
+)
+const countries = [ "USA", "UK", "Germany", "France", "Italy", "Japan", "China", "Russia", "India", "Australia", "Canada"];
+
+countries; 
+
+
+countries[6]; //? 
+
+countries //?
+
+function add (a, b) {
+
+    return a + b;
+}
+
+
+
+add(12, 15); //?
